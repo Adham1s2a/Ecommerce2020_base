9153df62ecea6248e2593f5249ed5d08d5044603
adham1s2a <adham1s2a@gmail.com>
2020-10-28 11:48:20 +0100

    multilanguage.js layout changes

---
+++ wwwroot/js/multilangauge.js
@@ -0,0 +1,49 @@
+﻿let english = document.getElementById("english");
+let deutsch = document.getElementById("deutsch");
+let title = document.getElementById("home");
+let contact = document.getElementById("contact");
+let login = document.getElementById("login");
+let register = document.getElementById("register");
+
+english.onclick = () => {
+    setLanguage("english");
+    localStorage.setItem("lang", "english");
+
+
+
+};
+
+deutsch.onclick = () => {
+
+    setLanguage("deutsch");
+    localStorage.setItem("lang", "deutsch");
+
+};
+
+onload = () => {
+
+    setLanguage(localStorage.getItem("lang"));
+
+}
+
+
+function setLanguage(getlanguage) {
+
+    if (getlanguage === "english") {
+
+        title.innerHTML = "Home";
+        contact.innerHTML = "Contact";
+        login.innerHTML = "login";
+        register.innerHTML = "Register";
+
+    } else if (getlanguage === "deutsch") {
+
+        title.innerHTML = "zu Hause";
+        contact.innerHTML = "Kontakt";
+        login.innerHTML = "Anmeldung";
+        register.innerHTML = "Registrieren";
+
+    }
+}
+
+
