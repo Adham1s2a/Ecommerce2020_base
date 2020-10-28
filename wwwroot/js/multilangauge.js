58fa7acbeb210b4c794b3bdc8bf1f36b329505f0
adham1s2a <adham1s2a@gmail.com>
2020-10-28 12:08:23 +0100

    dictionary for multilanguage.js

--- wwwroot/js/multilangauge.js
+++ wwwroot/js/multilangauge.js
@@ -4,6 +4,13 @@ let title = document.getElementById("home");
 let contact = document.getElementById("contact");
 let login = document.getElementById("login");
 let register = document.getElementById("register");
+let account = document.getElementById("account");
+let features_items = document.getElementById("features items");
+let category = document.getElementById("category");
+let recommended = document.getElementById("recommended");
+
+
+recommended
 
 english.onclick = () => {
     setLanguage("english");
@@ -35,13 +42,21 @@ function setLanguage(getlanguage) {
         contact.innerHTML = "Contact";
         login.innerHTML = "login";
         register.innerHTML = "Register";
+        account.innerHTML = "Account";
+        features_items.innerHTML = "Features items";
+        category.innerHTML = "Category";
+        recommended.innerHTML = "recommended items";
 
     } else if (getlanguage === "deutsch") {
 
-        title.innerHTML = "zu Hause";
+        title.innerHTML = "Home";
         contact.innerHTML = "Kontakt";
         login.innerHTML = "Anmeldung";
         register.innerHTML = "Registrieren";
+        account.innerHTML = "Konto";
+        features_items.innerHTML = "Top-Artikel";
+        category.innerHTML = "Kategorie";
+        recommended.innerHTML = "Empfohlene-Artikel";
 
     }
 }
