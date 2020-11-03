b57b567a6fb74c06df0e8801a319694ac41c2357
adham1s2a <adham1s2a@gmail.com>
2020-11-03 11:38:04 +0100

    langauges

--- wwwroot/js/multilangauge.js
+++ wwwroot/js/multilangauge.js
@@ -5,12 +5,14 @@ let contact = document.getElementById("contact");
 let login = document.getElementById("login");
 let register = document.getElementById("register");
 let account = document.getElementById("account");
-let features_items = document.getElementById("features items");
+let features_items = document.getElementById("features");
 let category = document.getElementById("category");
 let recommended = document.getElementById("recommended");
+let checkout = document.getElementById("checkout");
 
 
-recommended
+
+
 
 english.onclick = () => {
     setLanguage("english");
@@ -43,9 +45,11 @@ function setLanguage(getlanguage) {
         login.innerHTML = "login";
         register.innerHTML = "Register";
         account.innerHTML = "Account";
+        checkout.innerHTML = "Checkout";
         features_items.innerHTML = "Features items";
         category.innerHTML = "Category";
         recommended.innerHTML = "recommended items";
+        
 
     } else if (getlanguage === "deutsch") {
 
@@ -54,9 +58,11 @@ function setLanguage(getlanguage) {
         login.innerHTML = "Anmeldung";
         register.innerHTML = "Registrieren";
         account.innerHTML = "Konto";
+        checkout.innerHTML = "Prüfen";
         features_items.innerHTML = "Top-Artikel";
         category.innerHTML = "Kategorie";
         recommended.innerHTML = "Empfohlene-Artikel";
+        
 
     }
 }
